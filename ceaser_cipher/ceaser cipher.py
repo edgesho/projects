9c6412710495aeeb320d04bc47cf7287d03433e4
aditya27 <adityakudale2712@gmail.com>
2023-12-26 23:10:49 +0530

    CEASER CIPHER CODE

---
+++ ceaser_cipher/ceaser cipher.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 26 23:04:25 2023
+
+@author: aditya
+"""
+
+import art
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+   
+    if char not in alphabet:
+      end_text+=char
+    else:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+print(art.logo)
+
+restart=True
+while restart==True:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  if shift>26:
+    shift=shift%26
+    
+ 
+  
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  start=input("do you wish to continue? yes or no\n")
+  if start=="yes":
+    restart=True
+  else:
+    restart=False
+   
